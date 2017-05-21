b881289901cf19e78fba7e4e8ea900ebcd31560f
Olav <olav.kokovkin@gmail.com>
2017-05-21 18:27:27 +0300

    init

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div>my app</div>
+    );
+  }
+}
+App.defaultProps = {
+};
+App.propTypes = {
+};
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import './sass/styles.scss';
+
+const node = document.getElementById('react-root');
+ReactDOM.render(<App />, node);
